7df5cce81beecbef42d79dcbc9a07f86e0113cbc
umar <umars.salah@gmail.com>
2021-08-28 18:36:22 +0300

    Creat New Container Relates #6

--- Public/Script/dom.js
+++ Public/Script/dom.js
@@ -1,4 +1,5 @@
 const trendingTopics = document.querySelector('.TrendingTopics');
+const loading = document.querySelector('.Loading-container');
 
 const createResultedElement = (element, parent, classname) => {
   const item = document.createElement(element);
@@ -8,21 +9,29 @@ const createResultedElement = (element, parent, classname) => {
 };
 
 const createNewsCard = (response) => {
+  setTimeout(() => {
+    loading.classList.add('hidden');
+  }, 500);
   for (let topic = 0; topic <= 4; topic += 1) {
     const topicContainer = createResultedElement('div', trendingTopics, 'topicContainer');
-    topicContainer.style.backgroundImage = `url('${response[topic].image.url}')`;
+    const imgContainer = createResultedElement('div', topicContainer, 'img-container');
+    const img = createResultedElement('img', imgContainer, 'img');
+    img.src = response[topic].image.url;
     const title = createResultedElement('h1', topicContainer, 'title');
-    title.textContent = response[topic].title;
+    title.textContent = response[topic].title.substring(0, 25);
     const topicDetails = createResultedElement('div', topicContainer, 'topicDetails');
     const overview = createResultedElement('p', topicDetails, 'overview');
-    const textLimit = 50;
+    const textLimit = 25;
     const overviewText = response[topic].description;
-    if (overviewText.length > textLimit) {
+    if (overviewText.length > textLimit || title.length > textLimit) {
       const firstSet = overviewText.substring(0, textLimit);
       overview.innerText = `${firstSet}...`;
     } else {
       overview.innerText = overviewText;
     }
+    const readMore = createResultedElement('a', topicContainer, 'readMore');
+    readMore.href = response[topic].url;
+    readMore.innerText = 'Read More';
   }
 };
 
