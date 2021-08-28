ccba73043fe594d62bb00f02bbab124a3a1c4c7a
umar <umars.salah@gmail.com>
2021-08-28 19:13:48 +0300

    Test home and error routes Relates #10

--- Test/index.test.js
+++ Test/index.test.js
@@ -1,3 +1,25 @@
-test('Initial test', () => {
-  expect(2 + 2).toEqual(4);
+const request = require('supertest');
+
+const app = require('../Server/app');
+
+test('test 200 status /', (done) => {
+  request(app)
+    .get('/')
+    .expect(200)
+    .expect('Content-Type', /html/)
+    .end((err) => {
+      if (err) { return done(err); }
+      return done();
+    });
+});
+
+test('test  /404 route', (done) => {
+  request(app)
+    .get('/error')
+    .expect(404)
+    .expect('Content-Type', /html/)
+    .end((err) => {
+      if (err) { return done(err); }
+      return done();
+    });
 });
