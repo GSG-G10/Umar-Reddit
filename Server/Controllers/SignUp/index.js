06d824b7a59c415dc6210bde8628cd78baa1228d
umar <umars.salah@gmail.com>
2021-08-29 01:23:25 +0300

    Create sign up page Relates #14

---
+++ Server/Controllers/SignUp/index.js
@@ -0,0 +1,5 @@
+const { signUpPageHandler } = require('./SignUpPage');
+
+module.exports = {
+  signUpPageHandler,
+};
--- Server/Routes/index.js
+++ Server/Routes/index.js
@@ -3,12 +3,15 @@ const router = require('express').Router();
 const {
   TrendingTopicsAPI,
   loginPageHandler,
+  signUpPageHandler,
   clientError,
   serverError,
 } = require('../Controllers');
 
 router.get('/trending_topics', TrendingTopicsAPI);
-router.get('/loginPage', loginPageHandler);
+router.get('/login', loginPageHandler);
+router.get('/signup', signUpPageHandler);
+
 router.use(clientError);
 router.use(serverError);
 
