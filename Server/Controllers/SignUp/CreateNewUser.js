2fdd977c101fd2e079288da6a608014eb065a487
umar <umars.salah@gmail.com>
2021-08-30 15:08:41 +0300

    Hash Password in the DB Relates #22

--- Server/Controllers/SignUp/CreateNewUser.js
+++ Server/Controllers/SignUp/CreateNewUser.js
@@ -1,8 +1,10 @@
 const { addNewUser, getUserId } = require('../../Database/Queries');
+const { hashPassword } = require('../../Utills/Validations');
 
 const createNewUser = (req, res, next) => {
   const { username, email, password } = req.body.value;
-  addNewUser(username, email, password)
+  hashPassword(password)
+    .then((hashedPassword) => addNewUser(username, email, hashedPassword))
     .then(() => getUserId(email))
     .then((result) => {
       req.body.userId = result.rows[0].id;
