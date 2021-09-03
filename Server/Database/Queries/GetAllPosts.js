8b04818616156dba7ffe447de9f0ae4c3ff477d6
umar <umars.salah@gmail.com>
2021-09-03 23:54:19 +0300

    Edit posts Query Relates #50

--- Server/Database/Queries/GetAllPosts.js
+++ Server/Database/Queries/GetAllPosts.js
@@ -2,7 +2,7 @@ const connection = require('../Connection');
 
 module.exports = () => connection
   .query(`SELECT COUNT(comments.id) ,posts.id,title,posts.content,username
- FROM POSTS INNER JOIN users ON (posts.user_id = users.id) 
- JOIN comments ON (comments.post_id = posts.id)
-  GROUP BY posts.id,title,posts.content,username
-   ORDER BY posts.id DESC ;`);
+  FROM POSTS INNER JOIN users ON (posts.user_id = users.id) 
+  LEFT JOIN comments ON (comments.post_id = posts.id)
+   GROUP BY posts.id,title,posts.content,username
+    ORDER BY posts.id DESC ;`);
